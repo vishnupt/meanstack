75819ab9e0c8603e9255c525176c0b2754644866
Vishnu <vishnu@classyserve.com>
2019-05-31 14:24:21 +0530

    f

--- api/DB.js
+++
@@ -1,5 +0,0 @@
-var host=process.env.DB_HOST||'localhost'
-console.log(host);
-module.exports = {
-    DB: 'mongodb+srv://mongouser:1qazxsw2@cluster0-th4wg.mongodb.net/test?retryWrites=true&w=majority'
- };
